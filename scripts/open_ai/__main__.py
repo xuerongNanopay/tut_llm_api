0e1144995704b9ad4a1ad92d1a86f57e335e2cb5
xuerongNanopay <xuerong@nanopay.net>
2026-04-09 17:14:23 -0400

    add script for skill

--- scripts/open_ai/__main__.py
+++ scripts/open_ai/__main__.py
@@ -1,7 +1,11 @@
 # CLI for Open AI API.
 
-import requests
 import os
+import io
+import sys
+from pathlib import Path
+import zipfile
+import requests
 from urllib.parse import urljoin
 from dotenv import load_dotenv
 
@@ -16,7 +20,7 @@ headers = {
     "Authorization": f"Bearer {API_KEY}"
 }
 
-def list_skills():
+def list_agent_skills():
     response = requests.get(
         API_HOST_SKILL,
         headers=headers
@@ -24,6 +28,37 @@ def list_skills():
 
     print(response.json())
 
+def create_agent_skill(relative_path: str):
+    path = Path(relative_path).expanduser().resolve()
+    zip_buffer = zip_folder_to_memory(path)
+    folder_name = os.path.basename(os.path.normpath(path))
+
+    response = requests.post(
+        API_HOST_SKILL,
+        headers=headers,
+        files={
+            "files": (f"{folder_name}.zip", zip_buffer, "application/zip")
+        }
+    )
+    print(response.content)
+
+def zip_folder_to_memory(folder_path: Path) -> io.BytesIO:
+    buffer = io.BytesIO()
+
+    with zipfile.ZipFile(buffer, "w", zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                full_path = Path(root) / file
+                # 👇 include top-level folder
+                arcname = full_path.relative_to(folder_path.parent)
+                zipf.write(full_path, arcname)
+
+    buffer.seek(0)
+    return buffer
+
 
 if __name__ == "__main__":
-    list_skills()
+    list_agent_skills()
+
+    # input_path = sys.argv[1]
+    # create_agent_skill(input_path)
---
+++ skills/basic_math/SKILL.md
@@ -0,0 +1,6 @@
+---
+name: basic-math
+description: Add or multiply numbers.
+---
+
+Use this skill when you need a quick sum or product of numbers.
