25391e860711adb2376c2b9372ed5737b1f4c0ac
xuerongNanopay <xuerong@nanopay.net>
2026-04-09 17:37:10 -0400

    add method to print zip contents

--- scripts/open_ai/__main__.py
+++ scripts/open_ai/__main__.py
@@ -35,6 +35,15 @@ def retrieve_skill(skill_id: str):
 
     print(response.content)
 
+def retrieve_skill_content(skill_id: str):
+    response = requests.get(
+        f"{API_HOST_SKILL}/{skill_id}/content",
+        headers=headers
+    )
+
+    print_zip_contents(response.content)
+
+
 def create_agent_skill(relative_path: str):
     path = Path(relative_path).expanduser().resolve()
     zip_buffer = zip_folder_to_memory(path)
@@ -63,10 +72,32 @@ def zip_folder_to_memory(folder_path: Path) -> io.BytesIO:
     buffer.seek(0)
     return buffer
 
+def print_zip_contents(zip_bytes: bytes):
+    buffer = io.BytesIO(zip_bytes)
+
+    with zipfile.ZipFile(buffer, "r") as zipf:
+        for name in zipf.namelist():
+            print(f"\n>>>>>> {name}")
+
+            # skip directories
+            if name.endswith("/"):
+                continue
+
+            with zipf.open(name) as f:
+                try:
+                    content = f.read().decode("utf-8")
+                except UnicodeDecodeError:
+                    content = "<binary content>"
+                
+                print(content)
+
+            print(f"\n====== {name}")
+
 
 if __name__ == "__main__":
     # list_agent_skills()
 
-    retrieve_skill("skill_69d8160e358081909df65d25015f00d8001c67d04a78726c");
+    retrieve_skill("skill_69d8160e358081909df65d25015f00d8001c67d04a78726c")
+    retrieve_skill_content("skill_69d8160e358081909df65d25015f00d8001c67d04a78726c")    
     # input_path = sys.argv[1]
     # create_agent_skill(input_path)
