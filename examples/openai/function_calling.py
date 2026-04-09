302802b59d51c27737b74889a3da36965d1b91bd
xuerongNanopay <xuerong@nanopay.net>
2026-04-09 13:50:40 -0400

    add example for custom regex

--- examples/openai/function_calling.py
+++ examples/openai/function_calling.py
@@ -152,9 +152,33 @@ def tool_custom_lark():
 
     print(response.output)
 
+def tool_custom_regex():
+    grammar = r"^(?P<month>January|February|March|April|May|June|July|August|September|October|November|December)\s+(?P<day>\d{1,2})(?:st|nd|rd|th)?\s+(?P<year>\d{4})\s+at\s+(?P<hour>0?[1-9]|1[0-2])(?P<ampm>AM|PM)$"
+
+    tools = [
+        {
+            "type": "custom",
+            "name": "timestamp",
+            "description": "Saves a timestamp in date + time in 24-hr format.",
+            "format": {
+                "type": "grammar",
+                "syntax": "regex",
+                "definition": grammar
+            }
+        }
+    ]
+
+    response = client.responses.create(
+        model=default_gpt_model,
+        input="Use the timestamp tool to save a timestamp for August 7th 2025 at 10AM.",
+        tools=tools
+    )
+
+    print(response.output)
 
 if __name__ == "__main__":
     # tool_basic()
     # tool_stream()
     # tool_custom()
-    tool_custom_lark()
+    # tool_custom_lark()
+    tool_custom_regex()
